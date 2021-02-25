ac1795f9a7b76abda2b0f267ed88c40a6f923672
doyeon <doyeon311@gmail.com>
2021-02-25 14:10:36 +0900

    구현: 그리기 모드 버튼

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
 const range = document.getElementById("jsRange");
+const mode = document.getElementById("jsMode");
 
 canvas.width = 700;
 canvas.height = 700;
@@ -9,6 +10,7 @@ canvas.height = 700;
 ctx.strokeStyle = "#2c2c2c";
 ctx.lineWidth = 2.5;
 let painting = false;
+let filling = false;
 
 function stopPainting() {
   painting = false;
@@ -41,6 +43,16 @@ function handleRangeChange(event) {
   ctx.lineWidth = size;
 }
 
+function handelModeClick() {
+  if (filling == true) {
+    filling = false;
+    mode.innerHTML = "Fill";
+  } else {
+    filling = true;
+    mode.innerHTML = "Paint";
+  }
+}
+
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
   canvas.addEventListener("mousedown", startPainting);
@@ -54,4 +66,8 @@ if (colors) {
 
 if (range) {
   range.addEventListener("input", handleRangeChange);
+}
+
+if (mode) {
+  mode.addEventListener("click", handelModeClick);
 }
